b3696facca003d5dd61a6fa07d142fa385531390
Prince Iwejuo <princeiwejuo@gmail.com>
2020-12-11 13:50:53 -0500

    Sprint2

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+import Character from './components/Character';
+// import styled from 'styled-components'
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
-
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+  const [data, setData] = useState([])
+  useEffect(() => {
+    axios
+      .get('https://swapi.dev/api/people/')
+      .then(res => {
+        console.log('get', res.data.results);
+        setData(res.data.results);
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }, []);
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <div className="name">
+        {data.map((person) => {
+          return (
+            <Character
+              key={person.name}
+              name={person.name}
+              year={person.birth_year}
+              films={person.films.length}
+              ships={person.starships} />
+          )
+        })}
+      </div>
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,31 @@
 // Write your Character component here
+import React from "react"
+import styled from "styled-components"
+
+export default function Character(props) {
+    console.log('look', props)
+
+    return (
+        <CharacterStyle>
+            <Name>{props.name}</Name>
+            <h2>{props.birth_year}</h2>
+            <div className="bottom">
+                <p>Has been in {props.films} movies</p>
+                <p>Starships{props.starships}</p>
+                <p>Vehicles</p>
+            </div>
+        </CharacterStyle>
+    )
+};
+
+const CharacterStyle = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background: #529752;
+`
+const Name = styled.h2`
+    color: red;
+    font-size: bolder;
+    opacity: 50%;
+`
